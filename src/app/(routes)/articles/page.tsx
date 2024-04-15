495820f8459b0597138d4f53988c434bd35cf8fe
User <bruksbond@gmail.com>
2024-04-15 20:27:00 +0300

    add blog cart

---
+++ src/app/(routes)/articles/page.tsx
@@ -0,0 +1,13 @@
+import Articles from "@/shared/modules/articles";
+import Hero from "@/shared/modules/hero";
+
+const Page = () => {
+  return (
+    <div>
+      <Hero />
+      <Articles />
+    </div>
+  );
+};
+
+export default Page;
